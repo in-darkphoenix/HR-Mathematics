f64f465a0fe785c6990c3dc016a7297560679a26
Ankit <ph.official.ankit@gmail.com>
2020-10-09 14:08:37 +0530

    Max draws

---
+++ HRMaths/src/p1/MaxDraws.java
@@ -0,0 +1,14 @@
+package p1;
+//https://www.hackerrank.com/challenges/maximum-draws/problem
+
+public class MaxDraws {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static int maximumDraws(int n) {
+		return n + 1;
+	}
+
+}
